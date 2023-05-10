6021cb17a616eb837d44c65ad55aa5b48169bd7d
kanikaswe <atj0034237@est.univalle.edu>
2023-05-10 16:04:26 -0400

    nuestro primer commit

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace xd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            int result = Sum(2, 3);
+        }
+        public static int Sum(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+    }
+}
