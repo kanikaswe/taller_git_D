52273b626af530f4d07ff85d63381751562dea9f
kanikaswe <atj0034237@est.univalle.edu>
2023-05-10 16:10:28 -0400

    segundo commit

--- Program.cs
+++ Program.cs
@@ -8,10 +8,15 @@ namespace xd
         {
             Console.WriteLine("Hello, World!");
             int result = Sum(2, 3);
+            string nombrexd = nombre("jorge", "aguilar");
         }
         public static int Sum(int n1, int n2)
         {
             return n1 + n2;
+        }
+        public static string nombre(string name, string lastname)
+        {
+            return name + lastname;
         }
     }
 }
